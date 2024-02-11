5f8bb97d897512e2edd4b52b53d13f65971d7fc2
Sree <you@example.com>
2024-02-11 18:10:01 -0500

    stuff

--- Projects Main Page/Projects/5/script.js
+++ Projects Main Page/Projects/5/script.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var learnMoreBtn = document.getElementById("learnMoreBtn");
-    var learnMoreText = document.getElementById("learnMoreText");
-    var closeBtn = document.getElementById("closeBtn");
-  
-    learnMoreBtn.addEventListener("click", function() {
-      learnMoreText.classList.toggle("hidden");
-    });
-  
-    closeBtn.addEventListener("click", function() {
-      learnMoreText.classList.add("hidden");
-    });
-  });
-  
+function toggleContent(id) {
+  var content1 = document.getElementById('content1');
+  var content2 = document.getElementById('content2');
+
+  if (id === '1' || id === 1) {
+    content1.classList.add('active');
+    content2.classList.remove('active');
+  } else if (id === '2' || id === 2) {
+    content1.classList.remove('active');
+    content2.classList.add('active');
+  }
+}
+
+
+function changeImage() {
+  var sliderValue = document.getElementById('sliderInput').value;
+  var image = document.getElementById('sliderImage');
+  image.src = 'images/yoga' + sliderValue + '.jpg';
+}
+
